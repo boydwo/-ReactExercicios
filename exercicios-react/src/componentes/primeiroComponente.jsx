1dea8624f67fd1df21ee705ab5417712f50edffa
boydwo <marcos.t@ufv.br>
2019-12-26 19:31:52 -0300

    primeiroComponente

--- exercicios-react/src/componentes/primeiroComponente.jsx
+++ exercicios-react/src/componentes/primeiroComponente.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-function primeiro() {
-  return <h1>Primeiro Componente</h1>;
-}
+// function primeiro() {
+//   return <h1>Primeiro Componente</h1>;
+// }
+// export default primeiro;
 
-export default primeiro;
+export default () => <h1>Primeiro Componente</h1>;
