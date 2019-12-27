749e6bdff2ea64aa9cf331e1625712355ca15b04
boydwo <marcos.t@ufv.br>
2019-12-27 19:23:01 -0300

    commit de sexta

---
+++ exercicios-react/src/componentes/contador.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class Contador extends Component {
+  state = {
+    numero: 0
+  };
+
+  maisUm = () => {
+    this.setState({ numero: this.state.numero + 1 });
+  };
+  menosUm = () => {
+    this.setState({ numero: this.state.numero - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <div>Numero: {this.state.numero}</div>
+        <button onClick={this.maisUm}>Inc</button>
+        <button onClick={this.menosUm}>Dec</button>
+      </div>
+    );
+  }
+}
+
+//Solução 1 - Bind
+// constructor(props) {
+//   super(props);
+//   this.maisUm = this.maisUm.bind(this);
+// }
+
+// Solução2 - função arrow(onCLick)
+//<button onClick={() => this.maisUm()}>Inc</button>
+
+//solução 3 - função arrow
+// maisUm = () => {
+//   this.props.numero++;
+// }
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,13 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import PrimeiroComponente from "./componentes/primeiroComponente";
 //import { CompA, CompB as B } from "./componentes/doisComponentes";
-import MultiElementos from "./componentes/multiElementos";
-import FamiliaSilva from "./componentes/familiaSilva";
+// import MultiElementos from "./componentes/multiElementos";
+// import FamiliaSilva from "./componentes/familiaSilva";
+// import ComponenteComFuncao from "./componentes/componenteComFuncao";
+// import Pai from "./componentes/pai";
+// import ComponenteClasse from "./componentes/componeteClasse";
+import Contador from "./componentes/contador";
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
     {
-      <FamiliaSilva />
+      <Contador numero={0} />
+
+      // <ComponenteClasse valor='sou componente classe' />
+      // <Pai />
+      // <ComponenteComFuncao />
+      // <FamiliaSilva />
       /* { <CompA valor='Eu sou o A' />
     <B valor='Eu sou o B' />
     <PrimeiroComponente valor='bom dia' />} */
