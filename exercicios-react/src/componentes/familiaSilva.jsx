977f2cff4df50c398f585b1b0808d8069bc55e7a
boydwo <marcos.t@ufv.br>
2019-12-27 09:59:16 -0300

    familia

---
+++ exercicios-react/src/componentes/familiaSilva.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Membro from "./membro";
+
+export default props => (
+  <div>
+    <Membro nome='Rafael' sobrenome='Silva' />
+    <Membro nome='Daniela' sobrenome='Silva' />
+  </div>
+);
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 //import PrimeiroComponente from "./componentes/primeiroComponente";
 //import { CompA, CompB as B } from "./componentes/doisComponentes";
 import MultiElementos from "./componentes/multiElementos";
+import FamiliaSilva from "./componentes/familiaSilva";
 const elemento = document.getElementById("root");
 ReactDOM.render(
   <div>
     {
-      <MultiElementos />
+      <FamiliaSilva />
       /* { <CompA valor='Eu sou o A' />
     <B valor='Eu sou o B' />
     <PrimeiroComponente valor='bom dia' />} */
